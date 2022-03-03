67fc9993b1832096a7e91d1c2342c13ce2a3bded
Ramy <mouhamedrami.bendhia@esprit.tn>
2022-03-03 23:09:31 +0100

    test1

--- src/drapo/dashboard/HomeController.java
+++ src/drapo/dashboard/HomeController.java
@@ -7,6 +7,8 @@ package drapo.dashboard;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -25,42 +27,54 @@ import javafx.scene.layout.VBox;
  * @author oXCToo
  */
 public class HomeController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
-      @FXML
-    private VBox pnl_scroll;
-    
+
     @FXML
-    private void handleButtonAction(MouseEvent event) {        
-       refreshNodes();
+    private VBox pnl_scroll;
+
+    @FXML
+    private void handleButtonAction(MouseEvent event) {
+        refreshNodes();
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
-         refreshNodes();
-    }    
-    
-    private void refreshNodes()
-    {
+
+        refreshNodes();
+    }
+
+    private void refreshNodes() {
         pnl_scroll.getChildren().clear();
-        
-        Node [] nodes = new  Node[15];
-        
-        for(int i = 0; i<10; i++)
-        {
+
+        Node[] nodes = new Node[15];
+
+        List<String> list = new ArrayList<String>();
+        list.add("hana");
+        list.add("nhebeeeek");
+        list.add("barcha");
+        list.add("barcha");
+        list.add("barcha");
+
+        for (int i = 0; i < 10; i++) {
             try {
-                nodes[i] = (Node)FXMLLoader.load(getClass().getResource("Item.fxml"));
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Item.fxml"));
+                
+                ItemController cont = new ItemController();
+                cont.test="testtingg";
+                loader.setController(cont);
+
+                nodes[i] = (Node) loader.load();
+
+//                nodes[i] = (Node)FXMLLoader.load(getClass().getResource("Item.fxml"));
                pnl_scroll.getChildren().add(nodes[i]);
                 
             } catch (IOException ex) {
                 Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
             }
-           
-        }  
+        }
     }
-    
+
 }
